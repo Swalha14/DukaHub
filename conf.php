6297fde990359cb5ed87bfb85b3daf1a3f35fdb3
SajidaDahir <sajidadahirsheikh@gmail.com>
2025-09-22 12:08:25 +0300

    Testing Email

--- conf.php
+++ conf.php
@@ -3,3 +3,14 @@
 $conf['site_name']   = 'DukaHub';
 $conf['site_url']    = 'http://localhost/DUKAHUB';
 $conf['admin_email'] = 'admin@dukahub.com';
+
+// Email Configuration
+$conf = [
+    'site_name' => 'Dukahub',
+
+    // SMTP settings
+    'smtp_host' => 'smtp.gmail.com',
+    'smtp_user' => 'user email',
+    'smtp_pass' => '', // Gmail app password
+    'smtp_port' => 465
+];
