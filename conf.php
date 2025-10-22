9537cc076b40464a93f533fdbe20053422e71174
Swalha <swalhambarak887@gmail.com>
2025-10-22 10:44:06 +0300

    Renaming image

--- conf.php
+++ conf.php
@@ -10,22 +10,22 @@ $conf = [
 
     // SMTP settings
     'smtp_host' => 'smtp.gmail.com',
-    'smtp_user' => '',
-    'smtp_pass' => '', // Gmail app password
+    'smtp_user' => 'swalha.ahmed@strathmore',
+    'smtp_pass' => 'sqkt aqdt fkwd nhwo', // Gmail app password
     'smtp_port' => 465
 ];
 
 // Database Configuration
 $conf['db_type'] = 'pdo';
 $conf['db_host'] = 'localhost';
-//$conf['db_port'] = 3307; port={$conf['db_port']};
+$conf['db_port'] = 3307; 
 $conf['db_user'] = 'root';
-$conf['db_pass'] = '';
+$conf['db_pass'] = 'Swalha2006';
 $conf['db_name'] = 'dukahub';
 
 // Database Connection
 try {
-    $dsn = "mysql:host={$conf['db_host']};dbname={$conf['db_name']};charset=utf8mb4";
+    $dsn = "mysql:host={$conf['db_host']};port={$conf['db_port']};dbname={$conf['db_name']};charset=utf8mb4";
     $conn = new PDO($dsn, $conf['db_user'], $conf['db_pass']);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
